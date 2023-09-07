8b61918b97e455b9b77d8236598ee8b2b01b9316
MoonJihye <jihyer0395@gmail.com>
2023-09-07 14:07:10 +0900

    Fix: Missing class in interface

--- src/main/java/com/oss/gitborad/service/HashtagService.java
+++ src/main/java/com/oss/gitborad/service/HashtagService.java
@@ -9,6 +9,7 @@ public interface HashtagService {
     //Hashtag
     List<HashtagDTO.Info> findListByGroup(Long id);
     List<HashtagDTO.Info> findListByWriter(Long id);
+    HashtagDTO.GroupListInfo findHashtagByCertified(long categoryGroupId);
     List<HashtagDTO.Info> findAll(int pageNumber, int size);
     void saveHashtag(HashtagDTO.Request requestDTO);
     void deleteHashtag(Long id, Long userId);
