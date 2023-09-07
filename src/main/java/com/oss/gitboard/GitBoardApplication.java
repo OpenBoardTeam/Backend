f7125a3d4ca8a241e37c0ff1bafe9ddb217b6d26
MoonJihye <jihyer0395@gmail.com>
2023-09-07 21:45:33 +0900

    Refactor: 오탈자 수정

--- src/main/java/com/oss/gitboard/GitBoardApplication.java
+++ src/main/java/com/oss/gitboard/GitBoardApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GitBoradApplication {
+public class GitBoardApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GitBoradApplication.class, args);
+        SpringApplication.run(GitBoardApplication.class, args);
     }
 
 }
